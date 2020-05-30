8d371a4407bb0117dd61dec3705b43c3212c9d4b
É Machabée <mace2801@usherbrooke.ca>
2020-05-30 12:01:57 -0400

    Fix du main, application se lance maintenant

--- APP2 s3/src/main/java/Application/Main.java
+++ APP2 s3/src/main/java/Application/Main.java
@@ -2,40 +2,42 @@ package Application;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-//import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
 import java.io.IOException;
 
-public class Main extends Application {
-
-	private Stage primaryStage;
-	private BorderPane rootLayout;
-	
-	@Override
-	public void start(Stage primaryStage) {
-		
-		this.primaryStage = primaryStage;
+public class Main extends Application{
+    
+    private Stage primaryStage;
+    private Scene rootlayout;
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        this.primaryStage = primaryStage;
         this.primaryStage.setTitle("EMR Editor");
-        spawnWindow();
+
+        initRootLayout();
+
+        }
+    
+    
+
+    public void initRootLayout() {
+        try {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("vue.fxml"));
+        rootlayout = (Scene) loader.load();
+        primaryStage.setScene(rootlayout);
+        primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-	
-	public void spawnWindow() {
-		
-	    try {
-		    FXMLLoader loader = new FXMLLoader();
-		    loader.setLocation(Application.class.getResource("vue.fxml"));
-			rootLayout = (BorderPane) loader.load();
-			Scene scene = new Scene(rootLayout);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	public static void main(String[] args) {
-		launch(args);
-	}
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
 }
