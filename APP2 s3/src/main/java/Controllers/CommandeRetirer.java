56d35434d5d552da9c5d08f66423cc490b5bb96f
Martin Giroux <girm1002@usherbrooke.ca>
2020-06-01 13:56:16 -0400

    undo redo efface / remet une shape, faudrait revoir les stack et faire fonctionner avec les fleches

--- APP2 s3/src/main/java/Controllers/CommandeRetirer.java
+++ APP2 s3/src/main/java/Controllers/CommandeRetirer.java
@@ -12,57 +12,40 @@ public class CommandeRetirer implements Commande{
 	@Override
 	public void execute(InfosAjoutRetrait info, Canvas leCanvas, GraphicsContext aGC) {
 		aGC = leCanvas.getGraphicsContext2D();		
-		aGC.setFill(Color.LIGHTGREY);
-		aGC.setStroke(Color.LIGHTGREY);
+		aGC.setFill(Color.WHITE);
 		if(info.getTypeForme() == "carre")
 		{
-			aGC.strokeRect(info.getX(), info.getY(), info.getForme().getHauteur(), info.getForme().getLargeur());
-			aGC.fillRect(info.getX(), info.getY(), info.getForme().getHauteur(), info.getForme().getLargeur());
+			aGC.fillRect(info.getX()-1, info.getY()-1, info.getForme().getHauteur()+2, info.getForme().getLargeur()+2);
 		}
 		else if(info.getTypeForme() == "cercle")
 		{
-			aGC.strokeOval(info.getX(), info.getY(), info.getForme().getRayon(), info.getForme().getRayon());
-			aGC.fillOval(info.getX(), info.getY(), info.getForme().getRayon(), info.getForme().getRayon());
+			aGC.fillOval(info.getX()-1, info.getY()-1, info.getForme().getRayon()+2, info.getForme().getRayon()+2);
 		}
 		else if(info.getTypeForme() == "double_carre")
 		{
-			aGC.strokeRect(info.getX(), info.getY(), info.getForme().getHauteur(), info.getForme().getLargeur());
-			aGC.fillRect(info.getX(), info.getY(), info.getForme().getHauteur(), info.getForme().getLargeur());
-			aGC.strokeRect(info.getX()+15, info.getY()+15, info.getForme().getHauteur(), info.getForme().getLargeur());
-			aGC.fillRect(info.getX()+15, info.getY()+15, info.getForme().getHauteur(), info.getForme().getLargeur());
+			aGC.fillRect(info.getX()-1, info.getY()-1, info.getForme().getHauteur()+2, info.getForme().getLargeur()+2);
+			aGC.fillRect(info.getX()+14, info.getY()+14, info.getForme().getHauteur()+2, info.getForme().getLargeur()+2);
 		}
 		else if(info.getTypeForme() == "double_cercle")
 		{
-			aGC.strokeOval(info.getX(), info.getY(), info.getForme().getRayon(), info.getForme().getRayon());
-			aGC.fillOval(info.getX(), info.getY(), info.getForme().getRayon(), info.getForme().getRayon());
-			aGC.strokeOval(info.getX()+10, info.getY()+10, info.getForme().getRayon(), info.getForme().getRayon());
-			aGC.fillOval(info.getX()+10, info.getY()+10, info.getForme().getRayon(), info.getForme().getRayon());
+			aGC.fillOval(info.getX()-1, info.getY()-1, info.getForme().getRayon()+2, info.getForme().getRayon()+2);
+			aGC.fillOval(info.getX()+9, info.getY()+9, info.getForme().getRayon()+2, info.getForme().getRayon()+2);
 		}
 		else if(info.getTypeForme() == "rectangle_barre")
 		{
-			aGC.strokeRect(info.getX(), info.getY(), info.getForme().getLargeur(), info.getForme().getHauteur());
-			aGC.fillRect(info.getX(), info.getY(), info.getForme().getLargeur(), info.getForme().getHauteur());
-			aGC.strokeLine(info.getX(), info.getY()+info.getForme().getHauteur(), info.getX()+info.getForme().getLargeur(), info.getY());
+			aGC.fillRect(info.getX()-1, info.getY()-1, info.getForme().getLargeur()+2, info.getForme().getHauteur()+2);
 		}
 		else if(info.getTypeForme() == "source")
 		{
-			aGC.strokeOval(info.getX(), info.getY(), info.getForme().getLargeur(), info.getForme().getHauteur());
-			aGC.fillOval(info.getX(), info.getY(), info.getForme().getLargeur(), info.getForme().getHauteur());
-			aGC.strokeText("Source", info.getX()+info.getForme().getHauteur()/2, info.getY()+info.getForme().getHauteur()/2);
+			aGC.fillOval(info.getX()-1, info.getY()-1, info.getForme().getLargeur()+2, info.getForme().getHauteur()+2);
 		}
 		else if(info.getTypeForme() == "carre_sup")
 		{
-			aGC.strokeRect(info.getX(), info.getY(), info.getForme().getHauteur(), info.getForme().getLargeur());
-			aGC.fillRect(info.getX(), info.getY(), info.getForme().getHauteur(), info.getForme().getLargeur());
-			aGC.strokeLine(info.getX(), info.getY(), info.getX()+info.getForme().getHauteur(), info.getY()+info.getForme().getHauteur()/2);
-			aGC.strokeLine(info.getX(), info.getY()+info.getForme().getHauteur(), info.getX()+info.getForme().getHauteur(), info.getY()+info.getForme().getHauteur()/2);
+			aGC.fillRect(info.getX()-1, info.getY()-1, info.getForme().getHauteur()+2, info.getForme().getLargeur()+2);
 		}
 		else if(info.getTypeForme() == "carre_inf")
 		{
-			aGC.strokeRect(info.getX(), info.getY(), info.getForme().getHauteur(), info.getForme().getLargeur());
-			aGC.fillRect(info.getX(), info.getY(), info.getForme().getHauteur(), info.getForme().getLargeur());
-			aGC.strokeLine(info.getX()+info.getForme().getHauteur(), info.getY(), info.getX(), info.getY()+info.getForme().getHauteur()/2);
-			aGC.strokeLine(info.getX()+info.getForme().getHauteur(), info.getY()+info.getForme().getHauteur(), info.getX(), info.getY()+info.getForme().getHauteur()/2);
+			aGC.fillRect(info.getX()-1, info.getY()-1, info.getForme().getHauteur()+2, info.getForme().getLargeur()+2);
 		}	
 		
 	}
