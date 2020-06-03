6ea890a3884bf81eb43d2b76d73cd0eb52b60cb9
Martin Giroux <girm1002@usherbrooke.ca>
2020-06-03 18:14:43 -0400

    Réglé la factory du proto4

--- APP2 s3/src/main/java/models/FormeFactoryEnergy.java
+++ APP2 s3/src/main/java/models/FormeFactoryEnergy.java
@@ -7,36 +7,48 @@ public class FormeFactoryEnergy implements FormeFactory{
 	@Override
 	public Forme GetForme(eForme forme)
 	{
-		Forme f = new Forme();
-		f.setCouleur(Color.web("#FFD700"));
-		f.setBordure(Color.web("#FF0000"));
+		Forme f;
+		
 		
 		if (forme==eForme.CERCLE)
 		{
+			f = new Cercle();
 			f.setRayon(20);
+			f.setCouleur(Color.web("#FFD700"));
+			f.setBordure(Color.web("#FF0000"));
 		}
 		else if (forme==eForme.CARRE)
 		{
+			f = new Carre();
 			f.setHauteur(40);
 			f.setLargeur(40);
+			f.setCouleur(Color.web("#FFD700"));
+			f.setBordure(Color.web("#FF0000"));
 		}
 		else if (forme==eForme.RECTANGLE)
 		{
+			f = new Rectangle();
 			f.setHauteur(40);
 			f.setLargeur(20);
+			f.setCouleur(Color.web("#FFD700"));
+			f.setBordure(Color.web("#FF0000"));
 		}
 		else if (forme==eForme.SOURCE)
 		{
+			f = new source();
 			f.setCouleur(Color.web("#98FB98"));
 			f.setBordure(Color.web("#008000"));
 			f.setHauteur(30);
 			f.setLargeur(60);
 		}
+		else if (forme==eForme.FLECHE) {
+			f = new Fleche();
+			f.setBordure(Color.BLACK);
+		}
 		else
 		{
 			return null;
 		}
-		
 		return f;
 	}
 }
--- APP2 s3/src/main/java/models/FormeFactoryInversion.java
+++ APP2 s3/src/main/java/models/FormeFactoryInversion.java
@@ -1,35 +1,48 @@
 package models;
 import javafx.scene.paint.Color;
+import models.FormeFactory.eForme;
 
 public class FormeFactoryInversion implements FormeFactory{
 
 	@Override
 	public Forme GetForme(eForme forme)
 	{
-		Forme f = new Forme();
-		f.setCouleur(Color.web("#87CEEB"));
-		f.setBordure(Color.web("#0000FF"));
+		Forme f;
 		
 		if (forme==eForme.CERCLE)
 		{
+			f = new Cercle();
 			f.setRayon(20);
+			f.setCouleur(Color.web("#87CEEB"));
+			f.setBordure(Color.web("#0000FF"));
 		}
 		else if (forme==eForme.CARRE)
 		{
+			f = new Carre();
 			f.setHauteur(40);
 			f.setLargeur(40);
+			f.setCouleur(Color.web("#87CEEB"));
+			f.setBordure(Color.web("#0000FF"));
 		}
 		else if (forme==eForme.RECTANGLE)
 		{
+			f = new Rectangle();
 			f.setHauteur(40);
 			f.setLargeur(20);
+			f.setCouleur(Color.web("#87CEEB"));
+			f.setBordure(Color.web("#0000FF"));
 		}
 		else if (forme==eForme.SOURCE)
 		{
+			f = new source();
 			f.setCouleur(Color.web("#98FB98"));
 			f.setBordure(Color.web("#008000"));
 			f.setHauteur(30);
 			f.setLargeur(60);
+		}
+		else if (forme==eForme.FLECHE) {
+			f = new Fleche();
+			f.setBordure(Color.BLACK);
 		}
 		else
 		{
